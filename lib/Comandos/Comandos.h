72cfbb4f34733359977b563a0247a2674e71d028
robotaleh <alex.santos.ete@gmail.com>
2017-10-12 18:19:43 +0200

    Añade fichero de comandos

---
+++ lib/Comandos/Comandos.h
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Este fichero contendrá la lista completa de movimientos que realizará el robot,                        //
+// para gestionar facilmente la comunicación BT con el MiniFrankyBotController                            //
+//                                                                                                        //
+// NOTE: Este fichero debe estar siempre actualizado con su homólogo del proyecto MiniFrankyBotController //
+//       para mantener la integridad de los movimientos.                                                  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define BRAZO_DERECHO 'D'
+#define BRAZO_IZQUIERDO 'I'
